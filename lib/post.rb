4f4bd68a7aa63bc09697d9138dc93f008e5dcb0f
Pranik Chainani <pranikc@gmail.com>
2021-06-23 15:05:21 -0400

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,8 @@
+class Post 
+    attr_accessor :author, :title 
+
+    def initialize 
+        @title = "Latest Post"
+        @author = Author
+    end
+end
---
+++ lib/song.rb
@@ -0,0 +1,8 @@
+class Song
+    attr_accessor :title, :artist
+
+    def initialize
+        @title = "Latest song"
+        @artist = Artist
+    end
+end
